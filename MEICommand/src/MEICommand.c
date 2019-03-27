2587e9410dc296a18114ff8836c5cca9baf6641e
mark <mark@mark@kyin.org>
2019-03-27 07:59:33 -0400

    test

---
+++ MEICommand/src/MEICommand.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : MEICommand.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : Fireking Security Group
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("MEICommand"); /* prints MEICommand */
+	return EXIT_SUCCESS;
+}
