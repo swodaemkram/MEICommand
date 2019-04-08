75182071feb3475b34c6ec534b724402e1e8dda8
mark <mark@mark-desktop>
2019-04-07 20:15:32 -0400

    commit

--- MEICommand/src/build_packet_ext_cmd.c
+++ MEICommand/src/build_packet_ext_cmd.c
@@ -7,15 +7,13 @@ char *build_packet_ext_cmd(unsigned int pkt_command){
 
 
 
-	static char pkt[30] = "\x02\x10\x70\x00\x01\x10\x10\x01\x03"; //packet Skeleton
+	static char pkt[30] = "\x02\x10\x70\x00\x01\x00\x08\x01\x03"; //packet Skeleton
 
 			pkt[3] = pkt_command;						   //Pop command into packet Skeleton
 			int pkt_length = 10;	               //Get packet size
 			unsigned int crc_val;
 			crc_val = do_crc(pkt, pkt_length);             //Lets get crc of the packet
-
 			pkt[9] = crc_val;                 //Tac on crc to the end of the packet
-
 			printf("This is the EXT Packet I'm sending --> %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n\n",pkt[0],pkt[1],pkt[2],pkt[3],pkt[4],pkt[5],pkt[6],pkt[7],pkt[8],pkt[9]);
 
 return pkt;
